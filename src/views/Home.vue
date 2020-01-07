4783ae5348c564c3dc9504c35c344409911a4dca
Chris Bistline <cbistline@goprecise.com>
2020-01-07 09:14:49 -0600

    Initial upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+
+    <v-btn class="green white--text" depressed>
+      <v-icon left>mdi-email</v-icon>
+      <span>Email me</span>
+    </v-btn>
+    <br />
+    <br />
+    <v-btn small class="green white--text" depressed>
+      <v-icon left small>mdi-email</v-icon>
+      <span>Email me</span>
+    </v-btn>
+    <br />
+    <br />
+    <v-btn large class="green white--text" depressed>
+      <span>Email me</span>
+      <v-icon right large>mdi-email</v-icon>
+    </v-btn>
+    <br />
+    <br />
+    <v-btn depressed fab small dark color="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+    <br />
+    <br />
+    <v-text-field  label="Placeholder" outlined dense></v-text-field>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
